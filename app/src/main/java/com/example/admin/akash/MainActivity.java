e1fb4a881f6204051ed2d8f7e042bd8cf4af7dcd
spatils <spatils@gmail.com>
2018-10-24 20:03:13 +0530

    Base setup

--- app/src/main/java/com/example/admin/akash/MainActivity.java
+++ app/src/main/java/com/example/admin/akash/MainActivity.java
@@ -43,7 +43,7 @@ import me.relex.circleindicator.CircleIndicator;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+         {
     private  ViewPager mPager;
     private static int currentPage = 0;
     private static final Integer[] XMEN= {R.drawable.ic_menu_camera,R.drawable.ic_menu_send,R.drawable.ic_menu_manage,R.drawable.ic_menu_share,R.drawable.ic_menu_manage};
@@ -57,16 +57,13 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
         CardView  garmentAccessory = (CardView) findViewById(R.id.bankcardId);
         garmentAccessory.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,58 +132,8 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            final Context context = this;
-            Intent intent = new Intent(context, DisplayAccessory.class);
-            intent.putExtra("ACCESSORY","Garment Accessories");
-            startActivity(intent);
-
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
 
 
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
     public void selectDrawerItem(MenuItem menuItem) {
 
 
