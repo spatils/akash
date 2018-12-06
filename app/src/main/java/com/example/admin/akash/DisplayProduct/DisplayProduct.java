7bd917a3ae53db7cea00a94857ad540af1746472
spatils <spatils@gmail.com>
2018-12-06 18:46:10 +0530

    Zooming of product images

--- app/src/main/java/com/example/admin/akash/DisplayProduct/DisplayProduct.java
+++ app/src/main/java/com/example/admin/akash/DisplayProduct/DisplayProduct.java
@@ -7,13 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
+
+//import com.facebook.samples.zoomable.ZoomableDraweeView;
 import android.widget.TextView;
 
 import com.example.admin.akash.R;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.drawable.ProgressBarDrawable;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.samples.zoomable.ZoomableDraweeView;
 
 public class DisplayProduct extends AppCompatActivity {
 
@@ -33,11 +39,23 @@ public class DisplayProduct extends AppCompatActivity {
         TextView textViewProductName = (TextView) findViewById(R.id.productName);
         TextView textViewProductDescription = (TextView) findViewById(R.id.productDescription);
         Uri uri = Uri.parse(imageURL);
-        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.productImage);
-        draweeView.getHierarchy().setProgressBarImage(new ProgressBarDrawable());
-        draweeView.setImageURI(uri);
+
         textViewProductName.setText (productName);
         textViewProductDescription.setText(productDescription);
+
+        ZoomableDraweeView view = (ZoomableDraweeView) findViewById(R.id.zoomable);
+
+        DraweeController ctrl = Fresco.newDraweeControllerBuilder().setUri(
+                uri).setTapToRetryEnabled(true).build();
+        GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(getResources())
+                .setActualImageScaleType(ScalingUtils.ScaleType.FIT_CENTER)
+                .setProgressBarImage(new ProgressBarDrawable())
+                .build();
+
+        view.setController(ctrl);
+        view.setHierarchy(hierarchy);
+
+
 
     }
 
